c08e6ee053049bc5889c203b939af903e9827e02
alex yi <alexyi1234@berkeley.edu>
2019-10-26 18:58:56 -0700

    a form for limit

---
+++ app/screens/LimitForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+import { TextInput } from 'react-native';
+
+export default class Form extends Component {
+	/*static navigationOptions = {
+		title: 'Home',
+	};*/
+	onChanged(text){
+	    this.setState({
+	        mobile: text.replace(/[^0-9]/g, ''),
+	    });
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+			     <Text style={styles.headerText}>
+			     	Please enter your limit.
+        		</Text>
+
+				<TextInput 
+				   style={styles.textInput}
+				   keyboardType='numeric'
+				   onChangeText={(text)=> this.onChanged(text)}
+				   value={this.state.myNumber}
+				   maxLength={10}  //setting limit of input
+				/>      	
+
+			</View>
+		);
+		
+	};
+}
+
